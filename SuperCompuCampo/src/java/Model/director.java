5f5d8aa9021adfb97ab6c0658cd5a1d84e540451
Carlos Gerardo <Carlos Gerardo@10.33.197.230>
2014-11-19 13:52:35 -0600

    Se agregaron dos carpetas nuevas para la creación del Modelo MVC

---
+++ SuperCompuCampo/src/java/Model/director.java
@@ -0,0 +1,104 @@
+package Model;
+
+/**
+ *
+ * @author Carlos Gerardo
+ */
+public class director {
+
+    private int id_dir;
+    private String nombre_dir;
+    private String ap_dir;
+    private String am_dir;
+    private String cel_dir;
+    private String email_dir;
+
+    public director(
+            String nombre_dir,
+            String ap_dir,
+            String am_dir,
+            String cel_dir,
+            String email_dir) {
+
+        //this.id_dir = id_dir;
+        this.nombre_dir = nombre_dir;
+        this.ap_dir = ap_dir;
+        this.am_dir = am_dir;
+        this.cel_dir = cel_dir;
+        this.email_dir = email_dir;
+    }
+
+    public director() {
+
+    }
+
+    public int getid() {
+        return (id_dir);
+    }
+
+    public String getnombre() {
+        return (nombre_dir);
+    }
+
+    public String getap() {
+        return (ap_dir);
+    }
+
+    public String getam() {
+        return (am_dir);
+    }
+
+    public String getcelular() {
+        return (cel_dir);
+    }
+
+    public String getemail() {
+        return (email_dir);
+    }
+
+    public void setid(int id_dir) {
+        this.id_dir = id_dir;
+
+    }
+
+    public void setnombre(String nombre_dir) {
+        this.nombre_dir = nombre_dir;
+    }
+
+    public void setap(String ap_dir) {
+        this.ap_dir = ap_dir;
+    }
+
+    public void setam(String am_dir) {
+        this.am_dir = am_dir;
+    }
+
+    public void setcel(String cel_dir) {
+        this.cel_dir = cel_dir;
+    }
+
+    public void setemail(String email_dir) {
+        this.email_dir = email_dir;
+    }
+
+
+
+     
+     
+     
+     
+     
+     
+     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ SuperCompuCampo/src/java/Model/responsable.java
@@ -0,0 +1,75 @@
+package Model;
+
+/**
+ *
+ * @author Carlos Gerardo
+ */
+public class responsable {
+    
+    private int id_res;
+    private String nombre_res;
+    private String ap_res;
+    private String am_res;
+    private String cel_res;
+    
+    
+    public responsable(int id_res,
+            String nombre_res,
+            String ap_res,
+            String am_res,
+            String cel_res) {
+
+        this.id_res = id_res;
+        this.nombre_res = nombre_res;
+        this.ap_res = ap_res;
+        this.am_res = am_res;
+        this.cel_res = cel_res;
+        
+    }
+    
+     public responsable() {
+
+    }
+     
+      public int getid() {
+        return (id_res);
+    }
+
+    public String getnombre() {
+        return (nombre_res);
+    }
+
+    public String getap() {
+        return (ap_res);
+    }
+
+    public String getam() {
+        return (am_res);
+    }
+
+    public String getcelular() {
+        return (cel_res);
+    }
+    
+    public void setid(int id_res) {
+        this.id_res = id_res;
+
+    }
+
+    public void setnombre(String nombre_res) {
+        this.nombre_res = nombre_res;
+    }
+
+    public void setap(String ap_res) {
+        this.ap_res = ap_res;
+    }
+
+    public void setam(String am_res) {
+        this.am_res = am_res;
+    }
+
+    public void setcel(String cel_res) {
+        this.cel_res = cel_res;
+    }
+
+}
